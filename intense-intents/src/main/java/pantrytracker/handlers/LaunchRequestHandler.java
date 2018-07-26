87e20922293077af46f5f66ff8a3b55518088e0f
Mayukha Vadari <mvvadari@amazon.com>
2018-07-26 16:23:28 -0700

    Fixed launch request handler

--- intense-intents/src/main/java/pantrytracker/handlers/LaunchRequestHandler.java
+++ intense-intents/src/main/java/pantrytracker/handlers/LaunchRequestHandler.java
@@ -6,8 +6,10 @@ import com.amazon.ask.model.LaunchRequest;
 import com.amazon.ask.model.Response;
 import com.amazon.ask.request.Predicates;
 
+import pantrytracker.model.Attributes;
 import pantrytracker.model.Constants;
 
+import java.util.Map;
 import java.util.Optional;
  
 /**
@@ -22,6 +24,9 @@ public class LaunchRequestHandler implements RequestHandler {
  
      @Override
      public Optional<Response> handle(HandlerInput input) {
+    	 Map<String, Object> sessionAttributes = input.getAttributesManager().getSessionAttributes();
+    	 sessionAttributes.put(Attributes.STATE_KEY, Attributes.START_STATE);
+    	 
     	 return input.getResponseBuilder().withSpeech(Constants.START_MESSAGE).build();
      }
  
