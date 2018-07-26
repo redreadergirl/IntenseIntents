90220d6a345b8534bb14e2e109ef03a7a8e14b89
Wong <ncw@amazon.com>
2018-07-26 13:27:13 -0700

    add help intent

---
+++ intense-intents/src/main/java/pantrytracker/handlers/HelpIntentHandler.java
@@ -0,0 +1,27 @@
+package pantrytracker.handlers;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.Response;
+
+import java.util.Optional;
+
+import static com.amazon.ask.request.Predicates.intentName;
+
+public class HelpIntentHandler implements RequestHandler {
+
+    @Override
+    public boolean canHandle(HandlerInput input) {
+        return input.matches(intentName("AMAZON.HelpIntent"));
+    }
+
+    @Override
+    public Optional<Response> handle(HandlerInput input) {
+        String speechText = "You can update your inventory and make orders!";
+        return input.getResponseBuilder()
+                .withSpeech(speechText)
+                .withSimpleCard("IntenseIntents", speechText)
+                .withReprompt(speechText)
+                .build();
+    }
+}
