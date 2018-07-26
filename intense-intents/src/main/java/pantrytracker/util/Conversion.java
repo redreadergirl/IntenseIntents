7cb15098a7c30de4d751034bec9a0b77a3634ae5
Claire Romney <romnec@amazon.com>
2018-07-26 14:37:32 -0700

    add conversion

--- intense-intents/src/main/java/pantrytracker/util/Conversion.java
+++ intense-intents/src/main/java/pantrytracker/util/Conversion.java
@@ -1,4 +1,23 @@
 package pantrytracker.util;
 
+import org.apache.commons.logging.impl.WeakHashtable;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Conversion {
+    Map<String, Double> units;
+
+    public Conversion() {
+        units = new HashMap<String, Double>();
+        units.put("fluid ounce", 128.0);
+        units.put("gallon", 1.0);
+        units.put("pint", 8.0);
+        units.put("cup", 16.0);
+        units.put("quart", 4.0);
+    }
+
+    public double convert(String given, String needed, int amount) {
+        return
+    }
 }
