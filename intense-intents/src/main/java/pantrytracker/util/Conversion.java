c80aeca536e481c4350ffde1c1ec4c4fcbd59706
Claire Romney <romnec@amazon.com>
2018-07-26 14:50:51 -0700

    get conversion working

--- intense-intents/src/main/java/pantrytracker/util/Conversion.java
+++ intense-intents/src/main/java/pantrytracker/util/Conversion.java
@@ -11,13 +11,20 @@ public class Conversion {
     public Conversion() {
         units = new HashMap<String, Double>();
         units.put("fluid ounce", 128.0);
+        units.put("fluid ounces", 128.0);
         units.put("gallon", 1.0);
+        units.put("gallons", 1.0);
         units.put("pint", 8.0);
+        units.put("pints", 8.0);
         units.put("cup", 16.0);
+        units.put("cups", 16.0);
         units.put("quart", 4.0);
+        units.put("quarts", 4.0);
     }
 
     public double convert(String given, String needed, int amount) {
-        return
+        double ret = amount / units.get(given).doubleValue();
+        ret *= units.get(needed).doubleValue();
+        return ret;
     }
 }
